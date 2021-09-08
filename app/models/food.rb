25e49826162ed24dad94a3f3e01d7417822693a9
Stefan Koch <webokoch@gmx.de>
2021-09-08 17:01:20 +0200

    Added tests and validations to food model

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,2 +1,5 @@
 class Food < ApplicationRecord
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates :cuisine, presence: true
+  validates :description, presence: true, length: { maximum: 500 }
 end
--- test/models/food_test.rb
+++ test/models/food_test.rb
@@ -1,7 +1,38 @@
 require 'test_helper'
 
 class FoodTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @food = Food.new(name: "Sushi", cuisine: "japanese", description: "high class sushi")
+  end
+
+  test "should be valid" do
+    assert @food.valid?
+  end
+
+  test "name should be present" do
+    @food.name = " "
+    assert_not @food.valid?
+  end
+
+  test "name should be unique" do
+    duplicate_food = @food.dup
+    duplicate_food.name = @food.name.upcase
+    @food.save
+    assert_not duplicate_food.valid?
+  end
+
+  test "cuisine should be present" do
+    @food.cuisine = " "
+    assert_not @food.valid?
+  end
+
+  test "description should be present" do
+    @food.description = " "
+    assert_not @food.valid?
+  end
+
+  test "description must no be longer then 500 characters" do
+    @food.description = "a" * 501
+    assert_not @food.valid?
+  end
 end
