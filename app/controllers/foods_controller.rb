c3ed5e72afbb89deb5250e6a48a9bec8da59497f
Stefan Koch <webokoch@gmx.de>
2021-09-08 17:24:46 +0200

    Added food show action and view

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,15 @@
 class FoodsController < ApplicationController
-def index
-  @foods = Food.all
-end
+  before_action :set_food, only: [:show]
+
+  def index
+    @foods = Food.all
+  end
+
+  def show
+  end
+
+  private
+  def set_food
+    @food = Food.find(params[:id])
+  end
 end
