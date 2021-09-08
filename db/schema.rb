d0a65190a9ab28b331b18a38525ea6e9ca7bd4b4
Stefan Koch <webokoch@gmx.de>
2021-09-08 16:27:13 +0200

    Added Food model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_08_141913) do
+ActiveRecord::Schema.define(version: 2021_09_08_142605) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,14 @@ ActiveRecord::Schema.define(version: 2021_09_08_141913) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "foods", force: :cascade do |t|
+    t.string "name"
+    t.string "cuisine"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
