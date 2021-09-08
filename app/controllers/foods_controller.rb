f75871210778fd6457d6c26eef0f00ecebadae36
Stefan Koch <webokoch@gmx.de>
2021-09-08 17:35:47 +0200

    Added new & create action and new view

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -8,8 +8,26 @@ class FoodsController < ApplicationController
   def show
   end
 
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @food = Food.new(food_params)
+    if @food.save
+      redirect_to food_path(@food)
+    else
+      render :new
+    end
+
+  end
+
   private
   def set_food
     @food = Food.find(params[:id])
+  end
+
+  def food_params
+    params.require(:food).permit(:name, :cuisine, :description,)
   end
 end
