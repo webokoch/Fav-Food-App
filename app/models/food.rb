0f7a64272bedf86fb03b671cf31650f195b78695
Stefan Koch <webokoch@gmx.de>
2021-09-29 16:25:23 +0200

    added Review model and validations

--- app/models/food.rb
+++ app/models/food.rb
@@ -3,4 +3,5 @@ class Food < ApplicationRecord
   validates :cuisine, presence: true
   validates :description, presence: true, length: { maximum: 500 }
   has_one_attached :photo
+  has_many :reviews, dependent: :destroy
 end
