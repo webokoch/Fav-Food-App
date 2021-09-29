c7df0a8f63c5c9b0668712895acec7cf43964a13
Stefan Koch <webokoch@gmx.de>
2021-09-29 19:38:45 +0200

    added Average Rating to Review model

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,29 @@
 class ReviewsController < ApplicationController
+  before_action :set_food
+
   def new
     @food = Food.find(params[:food_id])
     @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @food = Food.find(params[:food_id])
+    @review.food = @food
+    if @review.save
+      redirect_to food_path(@food)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_food
+    @food = Food.find(params[:food_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
   end
 end
--- app/models/food.rb
+++ app/models/food.rb
@@ -4,4 +4,12 @@ class Food < ApplicationRecord
   validates :description, presence: true, length: { maximum: 500 }
   has_one_attached :photo
   has_many :reviews, dependent: :destroy
+
+  def average_rating
+    if reviews.size.positive?
+      reviews.average(:rating).round(2)
+    else
+      'undefined'
+    end
+  end
 end
