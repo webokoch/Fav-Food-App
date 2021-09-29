60d17add01ad9d83cb141ba5873de50c7ac7dfed
Stefan Koch <webokoch@gmx.de>
2021-09-29 16:31:04 +0200

    added Reviews controller & updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'foods#index'
-  resources :foods ,except: :index
+  resources :foods, except: :index do
+    resources :reviews, only: [ :new, :create ]
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
