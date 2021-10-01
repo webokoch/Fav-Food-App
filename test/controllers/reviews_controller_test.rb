eda58d5e993ed1c62a02967a4de4d0c6e6ebe4c7
Stefan Koch <webokoch@gmx.de>
2021-10-01 13:37:34 +0200

    minor chnages before adding pundit

--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -2,10 +2,24 @@ require 'test_helper'
 
 class ReviewsControllerTest < ActionDispatch::IntegrationTest
 
+  setup do
+    @food = foods(:food_001)
+  end
+
   test "should get new" do
     assert_routing '/foods/1/reviews/new',
       controller: "reviews",
       action: "new",
       food_id: "1"
   end
+
+  # test "should create a new review" do
+  #   assert_difference("Review.count") do
+  #     post food_reviews_url(@food), params: { review: { content: "A new test review",
+  #                                                       rating: "4"} }
+  #   end
+  #   assert_equal Review.last.rating, "4"
+  #   assert_redirected_to food_path(food_id: "1")
+  # end
 end
+
