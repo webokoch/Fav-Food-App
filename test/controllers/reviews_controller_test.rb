2c87f75dfda9020df85a90f790d8d8a6f795cbfd
Stefan Koch <webokoch@gmx.de>
2021-10-04 20:52:09 +0200

    added test for review#create

--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -3,6 +3,9 @@ require 'test_helper'
 class ReviewsControllerTest < ActionDispatch::IntegrationTest
 
   setup do
+    get '/users/sign_in'
+    sign_in users(:user_001)
+    post user_session_url
     @food = foods(:food_001)
   end
 
@@ -13,13 +16,14 @@ class ReviewsControllerTest < ActionDispatch::IntegrationTest
       food_id: "1"
   end
 
-  # test "should create a new review" do
-  #   assert_difference("Review.count") do
-  #     post food_reviews_url(@food), params: { review: { content: "A new test review",
-  #                                                       rating: "4"} }
-  #   end
-  #   assert_equal Review.last.rating, "4"
-  #   assert_redirected_to food_path(food_id: "1")
-  # end
+  test "should create a new review" do
+    assert_difference("Review.count") do
+      post food_reviews_url(@food), params: { review: { content: "A new test review",
+                                                        rating: 4,
+                                                        user_id: "1"} }
+    end
+    assert_equal Review.last.rating, 4
+    assert_redirected_to food_path(@food)
+  end
 end
 
